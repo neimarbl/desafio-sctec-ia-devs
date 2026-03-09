740cd0ff6e57353a7d26a25905e7a304a4c8ccbc
Neimar Bisewski Luiz <neimarbisewski@gmail.com>
2026-03-09 19:26:32 -0300

    feat: finaliza camada de dados e validacao

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models
+from database import SessionLocal, engine
+
+# Cria as tabelas no banco de dados automaticamente ao iniciar
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Empreendedorismo SC - API")
+
+# Função para obter uma conexão com o banco em cada requisição
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def home():
     return {"mensagem": "API de Empreendimentos de Santa Catarina operacional!"}
 
 @app.get("/status")
-def status():
-    return {"status": "online", "estado": "Santa Catarina"}
+def status(db: Session = Depends(get_db)):
+    return {"status": "online", "banco_conectado": True}
